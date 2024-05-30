0190db4636f4f8d7a98813fc2a824ec27eae6a37
Ivan Casavieja <ivanmichelcasavieja@gmail.com>
2024-05-30 01:22:30 -0300

    -

--- src/ItemListContainer.jsx
+++ src/ItemListContainer.jsx
@@ -48,7 +48,7 @@ export const ItemListContainer = () => {
       </ul>
       <div className='contenedorProductos'>
         {loading ? (
-          <img className='loading' src="/public/progress.gif" alt="Imagen de carga" />
+          <img className='loading' src="https://img.pikbest.com/png-images/20190918/cartoon-snail-loading-loading-gif-animation_2734139.png!sw800" alt="Imagen de carga" />
         ) : (
           data.map(productoData => {
             return <ItemList key={productoData.id} data={productoData} />;
